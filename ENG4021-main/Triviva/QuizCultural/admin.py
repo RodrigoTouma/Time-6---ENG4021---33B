980f33ce743a581fc72ad726fac6a09daca2296d
MatheusRaffaeli <matheusraffaeli8@icloud.com>
2025-10-30 13:22:43 -0700

    feat: register Pergunta model in admin
    Registered Pergunta model with custom admin options.

--- ENG4021-main/Triviva/QuizCultural/admin.py
+++ ENG4021-main/Triviva/QuizCultural/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from .models import Pergunta
 
-# Register your models here.
 
-from .models import MTCars
-
-admin.site.register(MTCars)
+@admin.register(Pergunta)
+class PerguntaAdmin(admin.ModelAdmin):
+    """Administração da classe Pergunta no painel do Django."""
+    list_display = ("texto", "categoria", "dificuldade")
+    search_fields = ("texto", "categoria")
