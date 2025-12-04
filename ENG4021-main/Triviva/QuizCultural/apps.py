3d3457b6f6148430d55a9356f2d649232e66e9c2
RodrigoTouma <rodrigotouma@gmail.com>
2025-12-04 19:35:30 +0000

    apps.py quiz

--- ENG4021-main/Triviva/QuizCultural/apps.py
+++ ENG4021-main/Triviva/QuizCultural/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class QuizCulturalConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Triviva.QuizCultural"
+    name = "QuizCultural"
